39f439ae0f304cad65f967580b15f707263c0f84
Chris Cardle <cdcardle@gmail.com>
2018-05-23 16:14:45 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -21,6 +21,7 @@ class SchoolClassesController < ApplicationController
   def update
     @class = SchoolClass.find(params[:id])
     @class.update(class_params(:title, :room_number))
+    redirect_to school_class_path(@class)
   end
 
   def edit
