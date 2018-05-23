9ed91bcfd06fd95cffae41c961f3680fb86daf89
Chris Cardle <cdcardle@gmail.com>
2018-05-23 12:05:55 +0000

    automatically backed up by learn

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,22 @@
+class SchoolClassesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  private
+
+  def puts_params(*args)
+    params.require(:)
+  end
+end
