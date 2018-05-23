c076de3d2e533b2bbc1f00b969cce90a413aeeff
Chris Cardle <cdcardle@gmail.com>
2018-05-23 14:04:55 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,14 +13,14 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.new(student_params(:title, :room_number))
+    @student = Student.new(student_params(:first_name, :last_name))
     @student.save
     redirect_to student_path(@student)
   end
 
   def update
     @student = Student.find(params[:id])
-    @student.update(student_params(:room_number))
+    @student.update(student_params(:last_name))
   end
 
   private
